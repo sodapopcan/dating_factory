5a43a5df27fe4a8b3d90d1cddcf19951b1e63bdc
Andrew Haust <andrewwhhaust@gmail.com>
2022-05-02 20:54:10 -0400

    Parse all sorts of times

--- lib/d.ex
+++ lib/d.ex
@@ -1,15 +1,22 @@
 defmodule D do
+  @time ~r/\A([0-9][0-9]?):?([0-6][0-9])?:?([0-6][0-9])?(am|pm)/
+
   def parse_time(string) do
-    [hour] = Regex.run(~r/\A[0-9][0-9]?/, string)
-    hour = String.to_integer(hour)
+    [hour, minute, second, period] = Regex.run(@time, string, capture: :all_but_first)
+    hour = to_integer(hour)
+    minute = to_integer(minute)
+    second = to_integer(second)
 
     hour =
-      if String.ends_with?(string, "pm") do
+      if period == "pm" do
         hour + 12
       else
         hour
       end
 
-    Time.new!(hour, 0, 0)
+    Time.new!(hour, minute, second)
   end
+
+  defp to_integer(""), do: 0
+  defp to_integer(string), do: String.to_integer(string)
 end
--- test/d_test.exs
+++ test/d_test.exs
@@ -8,5 +8,15 @@ defmodule DTest do
       assert parse_time("2pm") == Time.new!(14, 0, 0)
       assert parse_time("11am") == Time.new!(11, 0, 0)
     end
+
+    test "parses hour and minutes with am/pm" do
+      assert parse_time("4:04am") == Time.new!(4, 4, 0)
+      assert parse_time("10:04pm") == Time.new!(22, 4, 0)
+    end
+
+    test "parses hour, minutes, and seconds with am/pm" do
+      assert parse_time("11:04:56pm") == Time.new!(23, 4, 56)
+      assert parse_time("11:04:56am") == Time.new!(11, 4, 56)
+    end
   end
 end
